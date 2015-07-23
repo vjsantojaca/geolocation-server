9feeac4e0ba007f6f2475a66c88d293d2193f1bc
vjsantojaca <vsantoja@onwhyon.com>
2015-07-23 11:37:20 +0200

    Anotaciones para que sepa donde se encuentran las entidades y los repositorios

--- src/main/java/com/merinosa/geolocation/vjsantojaca/server/Application.java
+++ src/main/java/com/merinosa/geolocation/vjsantojaca/server/Application.java
@@ -1,8 +1,16 @@
 package com.merinosa.geolocation.vjsantojaca.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.merinosa.geolocation.vjsantojaca.server.models.repositories"})
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
