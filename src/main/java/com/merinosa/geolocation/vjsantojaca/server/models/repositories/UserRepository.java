ac58f1f3d013e9be871d3a2a77315cd6b72703be
vjsantojaca <vsantoja@onwhyon.com>
2015-07-24 13:34:41 +0200

    New function in user repository

--- src/main/java/com/merinosa/geolocation/vjsantojaca/server/models/repositories/UserRepository.java
+++ src/main/java/com/merinosa/geolocation/vjsantojaca/server/models/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.merinosa.geolocation.vjsantojaca.server.models.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.merinosa.geolocation.vjsantojaca.server.models.entities.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
+public interface UserRepository extends CrudRepository<UserEntity, Long> 
+{
+	List<UserEntity> findUserByNickAndPass(String nick, String pass);
+	List<UserEntity> findUserByNick(String nick);
 }
