9232938ee8fd2a3dc18c6e00bf11df8e5a7b6b2a
vjsantojaca <vsantoja@onwhyon.com>
2015-07-24 08:35:33 +0200

    Anotaciones a la hora de lanzar la app

--- src/main/java/com/merinosa/geolocation/vjsantojaca/server/Application.java
+++ src/main/java/com/merinosa/geolocation/vjsantojaca/server/Application.java
@@ -3,13 +3,20 @@ package com.merinosa.geolocation.vjsantojaca.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.merinosa.geolocation.vjsantojaca.server.models.repositories"})
 @ComponentScan
+@EntityScan(basePackages={"com.merinosa.geolocation.vjsantojaca.server.models.entities"})
+@EnableJpaRepositories(basePackages = {"com.merinosa.geolocation.vjsantojaca.server.models.repositories"})
+@PropertySources({ 
+    @PropertySource(value = "classpath:application.properties"), 
+}) 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
