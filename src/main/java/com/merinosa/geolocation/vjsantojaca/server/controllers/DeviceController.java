907727574cc7bb64d354f9ec73b68d45383af674
vjsantojaca <vsantoja@onwhyon.com>
2015-07-24 10:29:31 +0200

    New action in controllers

--- src/main/java/com/merinosa/geolocation/vjsantojaca/server/controllers/DeviceController.java
+++ src/main/java/com/merinosa/geolocation/vjsantojaca/server/controllers/DeviceController.java
@@ -1,5 +1,6 @@
 package com.merinosa.geolocation.vjsantojaca.server.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -14,9 +15,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Lists;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.BlockEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.CallEntity;
 import com.merinosa.geolocation.vjsantojaca.server.models.entities.DeviceEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.LocationEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.SMSEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.SystemMessageEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.BlockRepository;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.CallRepository;
 import com.merinosa.geolocation.vjsantojaca.server.models.repositories.DeviceRepository;
 import com.merinosa.geolocation.vjsantojaca.server.models.repositories.LocationRepository;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.SmsRepository;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.SystemMessageRepository;
 import com.merinosa.geolocation.vjsantojaca.server.responses.DeviceResponse;
 
 @RestController
@@ -28,6 +38,18 @@ public class DeviceController
 	
 	@Autowired
 	private LocationRepository locationRepository;
+	
+	@Autowired
+	private CallRepository callRepository;
+	
+	@Autowired
+	private SmsRepository smsRepository;
+	
+	@Autowired
+	private SystemMessageRepository systemMessageRepository;
+	
+	@Autowired
+	private BlockRepository blockRepository;
 	
 	@RequestMapping(method= RequestMethod.POST, headers = "content-type=application/json")
 	public ResponseEntity<String> newDevice (@RequestBody String request) {
@@ -45,6 +67,9 @@ public class DeviceController
 		if( findOne != null ) {
 			DeviceResponse device = new DeviceResponse(findOne);
 			device.setLocations(locationRepository.findLocationByIdUserOrderByDateDesc(device.getIdDevice()));
+			device.setCalls(callRepository.findCallByIdDeviceOrderDateDesc(device.getIdDevice()));
+			device.setSms(smsRepository.findSmsByIdDeviceOrderDateDesc(device.getIdDevice()));
+			device.setMessages(systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(device.getIdDevice()));
 			return new ResponseEntity<String>((new JSONObject(device)).toString(), HttpStatus.OK);
 		}
 		else {
@@ -52,6 +77,9 @@ public class DeviceController
 			 if ( findOne != null ) {
 				 DeviceResponse device = new DeviceResponse(findOne);
 				 device.setLocations(locationRepository.findLocationByIdUserOrderByDateDesc(device.getIdDevice()));
+				 device.setCalls(callRepository.findCallByIdDeviceOrderDateDesc(device.getIdDevice()));
+				 device.setSms(smsRepository.findSmsByIdDeviceOrderDateDesc(device.getIdDevice()));
+				 device.setMessages(systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(device.getIdDevice()));
 				 return new ResponseEntity<String>((new JSONObject(device)).toString(), HttpStatus.OK);
 			 }
 		}
@@ -61,19 +89,58 @@ public class DeviceController
 	@RequestMapping(value="/number",method= RequestMethod.GET)
 	public ResponseEntity<String> getDeviceByNumber (@RequestParam(value="number", required=true, defaultValue="0") int number) {
 		DeviceEntity findByNumber = deviceRepository.findByNumberDevice(number);
-		if( findByNumber != null ) 
-			return new ResponseEntity<String>((new JSONObject(findByNumber)).toString(), HttpStatus.OK);
-		
+		if( findByNumber != null ) {
+			DeviceResponse device = new DeviceResponse(findByNumber);
+			device.setLocations(locationRepository.findLocationByIdUserOrderByDateDesc(device.getIdDevice()));
+			device.setCalls(callRepository.findCallByIdDeviceOrderDateDesc(device.getIdDevice()));
+			device.setSms(smsRepository.findSmsByIdDeviceOrderDateDesc(device.getIdDevice()));
+			device.setMessages(systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(device.getIdDevice()));
+			return new ResponseEntity<String>((new JSONObject(device)).toString(), HttpStatus.OK);
+		}
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value="/name", method= RequestMethod.GET)
 	public ResponseEntity<String> getDevicesByName (@RequestParam(value="name", required=true, defaultValue="") String name) {
-		List<DeviceEntity> findByNumber = deviceRepository.findByNickDeviceOrNameDevice(name, name);
-		if( findByNumber != null && findByNumber.size() != 0 ) 
-			return new ResponseEntity<String>((new JSONArray(findByNumber)).toString(), HttpStatus.OK);
-		
+		List<DeviceEntity> findByName = deviceRepository.findByNickDeviceOrNameDevice(name, name);
+		if( findByName != null && findByName.size() != 0 ) {
+			List<DeviceResponse> devices = new ArrayList<>();
+			for( DeviceEntity dev : findByName ) {
+				DeviceResponse device = new DeviceResponse(dev);
+				device.setLocations(locationRepository.findLocationByIdUserOrderByDateDesc(device.getIdDevice()));
+				device.setCalls(callRepository.findCallByIdDeviceOrderDateDesc(device.getIdDevice()));
+				device.setSms(smsRepository.findSmsByIdDeviceOrderDateDesc(device.getIdDevice()));
+				device.setMessages(systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(device.getIdDevice()));
+				devices.add(device);
+			}
+			return new ResponseEntity<String>((new JSONArray(devices)).toString(), HttpStatus.OK);
+		}
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping(value="/delete", method= RequestMethod.POST, headers = "content-type=application/json")
+	public ResponseEntity<String> deleteDevice (@RequestBody String request) {
+		int numberDevice = (new JSONObject(request)).getInt("number");
+		
+		DeviceEntity device = deviceRepository.findByNumberDevice(numberDevice);
+		deviceRepository.delete((long) device.getIdDevice());
+		
+		List<LocationEntity> locationsByDevice = locationRepository.findLocationByIdUserOrderByDateDesc(numberDevice);
+		locationRepository.delete(locationsByDevice);
+		
+		List<SMSEntity> smsByDevice = smsRepository.findSmsByIdDeviceOrderDateDesc(numberDevice);
+		smsRepository.delete(smsByDevice);
+		
+		List<SystemMessageEntity> systemMessageDevice = systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(numberDevice);
+		systemMessageRepository.delete(systemMessageDevice);
+		
+		List<CallEntity> callByDevice = callRepository.findCallByIdDeviceOrderDateDesc(numberDevice);
+		callRepository.delete(callByDevice);
+		
+		BlockEntity blockDevice = blockRepository.findBlockyByIdDevice(numberDevice);
+		blockRepository.delete(blockDevice);
+		
+		return new ResponseEntity<String>(HttpStatus.OK);
 	}
 	
 //	@RequestMapping(value="/update", method= RequestMethod.POST, headers = "content-type=application/json")
@@ -84,7 +151,19 @@ public class DeviceController
 	@RequestMapping(value="/all", method= RequestMethod.GET)
 	public ResponseEntity<String> getListDevice () {
 		List<DeviceEntity> devices = Lists.newArrayList(deviceRepository.findAll());
-		return new ResponseEntity<String>((new JSONArray(devices)).toString(), HttpStatus.OK);
+		if( devices!= null ){
+			List<DeviceResponse> deviceResponse = new ArrayList<>();
+			for( DeviceEntity dev : devices ) {
+				DeviceResponse device = new DeviceResponse(dev);
+				device.setLocations(locationRepository.findLocationByIdUserOrderByDateDesc(device.getIdDevice()));
+				device.setCalls(callRepository.findCallByIdDeviceOrderDateDesc(device.getIdDevice()));
+				device.setSms(smsRepository.findSmsByIdDeviceOrderDateDesc(device.getIdDevice()));
+				device.setMessages(systemMessageRepository.findSystemMessageByIdDeviceOrderByDateDesc(device.getIdDevice()));
+				deviceResponse.add(device);
+			}
+			return new ResponseEntity<String>((new JSONArray(deviceResponse)).toString(), HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value="/checkPass", method= RequestMethod.POST, headers = "content-type=application/json")
@@ -94,9 +173,9 @@ public class DeviceController
 		int number = object.getInt("id");
 		
 		List<DeviceEntity> devices = deviceRepository.findByPassAndNumberDevice(pass, number);
-		if( devices != null  && devices.size() != 0 ) 
+		if( devices != null && devices.size() != 0 ) 
 		{
-//			deviceRepository.setGcmDeviceById(object.getString("model"), number);
+			deviceRepository.setGcmDeviceById(object.getString("model"), number);
 			return new ResponseEntity<String>(HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
@@ -111,7 +190,7 @@ public class DeviceController
 		DeviceEntity device = deviceRepository.findByNumberDevice(number);
 		if( device != null ) 
 		{
-//			deviceRepository.setGcmDeviceById(regId, number);
+			deviceRepository.setGcmDeviceById(regId, number);
 			return new ResponseEntity<String>(HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
--- src/main/java/com/merinosa/geolocation/vjsantojaca/server/controllers/LocationController.java
+++ src/main/java/com/merinosa/geolocation/vjsantojaca/server/controllers/LocationController.java
@@ -1,11 +1,24 @@
 package com.merinosa.geolocation.vjsantojaca.server.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
+import com.merinosa.geolocation.vjsantojaca.server.models.entities.LocationEntity;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.DeviceRepository;
+import com.merinosa.geolocation.vjsantojaca.server.models.repositories.LocationRepository;
+import com.merinosa.geolocation.vjsantojaca.server.responses.LocationResponse;
 
 
 /*
@@ -16,6 +29,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/api/location")
 public class LocationController 
 {
+	@Autowired
+	private DeviceRepository deviceRepository;
+	
+	@Autowired
+	private LocationRepository locationRepository;
+	
 	@RequestMapping(method= RequestMethod.POST, headers = "content-type=application/json")
 	public boolean geolocation ( @RequestBody String request ) {
 		
@@ -23,7 +42,27 @@ public class LocationController
 	}
 	
 	@RequestMapping(value="/last", method= RequestMethod.GET, headers = "content-type=application/json")
-	public ResponseEntity<String> getLastLocation() {
-		return new ResponseEntity<String>(HttpStatus.OK);
+	public ResponseEntity<String> getLastLocation() 
+	{
+		Iterable<LocationEntity> findAll = locationRepository.findLocationOrderByDateDesc();
+		List<LocationResponse> locationsResponse = new ArrayList<>();
+		List<LocationEntity> locations = Lists.newArrayList(findAll);
+		
+		if( locations != null && !locations.isEmpty() ) {
+			Map<Integer, LocationEntity> locationsMap = new HashMap();
+			for( LocationEntity location : locations ) {
+				if( !locationsMap.containsKey(location.getIdUser()) ) {
+					locationsMap.put(location.getIdUser(), location);
+				}
+			}
+			
+			for( LocationEntity location : locationsMap.values() ) {
+				LocationResponse locationResponse = new LocationResponse(deviceRepository.findByNumberDevice(location.getIdUser()), location);
+				locationsResponse.add(locationResponse);
+			}
+			
+			return new ResponseEntity<String>((new JSONArray(locationsResponse)).toString(), HttpStatus.OK);
+		} else
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);		
 	}
 }
